d930e1c576bafe1319df0a66f6886034a73f806f
donkolbe <dominic@DK-MacBook-Pro.local>
2015-02-27 01:06:35 +0100

    init
    init

---
+++ dev/javascripts/linkman.app.js
@@ -0,0 +1,44 @@
+angular.module('linkmanApp', ['firebase'])
+
+.controller('mainController', function($scope, $firebase) {
+
+
+        var ref = new Firebase("https://first-touch.firebaseio.com/links");
+        var fb = $firebase(ref);
+
+        var syncObject = fb.$asObject();
+
+        syncObject.$bindTo($scope, 'links');
+
+
+        $scope.reset = function() {
+
+            fb.$remove();
+
+        };
+
+    })
+    .controller('AddLinkCtrl', function($scope, $firebase) {
+
+
+        var ref = new Firebase("https://first-touch.firebaseio.com/links");
+        var fb = $firebase(ref);
+
+
+        var thistitle = $scope.title;
+        //var src   = $scope.src;
+
+
+        $scope.add = function() {
+
+            fb.$push({
+
+                title: thistitle,
+                src: 'abc'
+
+            });
+
+        };
+
+
+    })
