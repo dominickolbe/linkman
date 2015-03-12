62af1adc35a97dfb326efa643db78618aef86357
donkolbe <dominic@DK-MacBook-Pro.local>
2015-03-12 20:59:03 +0100

    rcea

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('./public'));
 
 app.get('*', function(req, res) {
 
-    res.render('index');
+    res.sendfile('public/index.html');
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+app.listen();
