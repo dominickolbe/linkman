0d9301cf1cb00768f0fcf5e049c97d5cdd2cb893
donkolbe <dominic@DK-MacBook-Pro.local>
2015-03-12 20:57:02 +0100

    wfwq

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// make express look in the public directory for assets (css/js/img)
+
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
 
-    // ejs render automatically looks in the views folder
     res.render('index');
 });
 
