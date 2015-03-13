92e6049fa6fbca0476398b3e38fdb7e1b16c19eb
Dominic Kolbe <dev@dominickolbe.dk>
2015-03-13 14:20:30 +0100

    dsfvd

---
+++ public/scripts/linkman.app.js
@@ -0,0 +1,83 @@
+angular.module('linkmanApp', ['ngAnimate', 'ui.router', 'firebase'])
+
+.constant('FBURL', "https://linkman.firebaseio.com/links")
+
+.factory('fbService', function($firebase, FBURL) {
+    var ref = new Firebase(FBURL);
+    return $firebase(ref);
+})
+
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $urlRouterProvider.otherwise('/links');
+
+    $stateProvider
+
+    .state('links', {
+        url: '/links',
+        templateUrl: 'views/link-list.html',
+        controller: 'ListController'
+    })
+
+    .state('links.add', {
+        url: '/add',
+        templateUrl: 'views/link-add.html',
+        controller: 'AddLinkController'
+    })
+
+    .state('links.edit', {
+        url: '/edit',
+        templateUrl: 'views/link.add.html'
+    });
+
+    $locationProvider
+        .html5Mode(true);
+})
+
+
+
+.controller('mainController', function($scope, fbService) {
+
+    $scope.reset = function() {
+        fbService.$remove();
+    };
+
+})
+
+.controller('ListController', function($scope, fbService) {
+
+    var syncObject = fbService.$asObject();
+
+    syncObject.$bindTo($scope, 'links');
+
+})
+
+.controller('AddLinkController', function($scope, fbService, $state) {
+
+    //$scope.fb = fbService;
+
+    $scope.submit = function() {
+
+        fbService.$push({
+
+            title: $scope.title,
+            src: $scope.url
+
+        });
+
+        $state.go('links');
+
+    };
+
+    $scope.cancel = function(){
+        $state.go('links');
+    }
+
+
+})
+
+.controller('EditController', function($scope, fbService) {
+
+
+
+});
